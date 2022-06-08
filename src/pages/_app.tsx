9b2228667616892d81ae2aa99d2c709320c61332
Bill Searle <bill.searle@oysterhr.com>
2022-06-08 13:07:43 +0100

    Add subpath demo

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Link from "next/link";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <nav>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/members">
+          <a>Members</a>
+        </Link>
+      </nav>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  const router = useRouter();
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <span className={styles.title}>basePath: {router.basePath}</span>
+        <span className={styles.title}>asPath: {router.asPath}</span>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
